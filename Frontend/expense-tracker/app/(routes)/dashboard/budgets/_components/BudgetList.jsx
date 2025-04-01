5c442480c6b5daf72917c24cc7e54d4979725bfd
KavyaBajpai <kavyabajpai05@gmail.com>
2025-04-01 23:11:20 +0530

    displaying budget lists

--- Frontend/expense-tracker/app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ Frontend/expense-tracker/app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState} from 'react'
 import CreateBudget from './CreateBudget'
-
+import { db } from '@/utils/dbConfig'
+import { useUser } from '@clerk/nextjs'
+import { eq, getTableColumns, sql } from 'drizzle-orm'
+import { Budgets, Expenses } from '@/utils/schema'
+import BudgetItem from './BudgetItem'
 function BudgetList() {
+  const [budgetList, setBudgetList] = useState([])
+  const {user} = useUser();
+  useEffect(() => {
+    getBudgetList()
+  },[user])
+  const getBudgetList = async () => {
+   
+    const result = await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend: sql`sum(${Expenses.amount}::integer)`.mapWith(Number),
+      totalItem: sql`count(${Expenses.id})`.mapWith(Number)
+    }).from(Budgets)
+    .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+    .where(eq(Budgets.createdBy, user.id))
+    .groupBy(Budgets.id)
+     setBudgetList(result);
+     console.log("hi")
+    console.log(result)
+  }
   return (
     <div>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
            <CreateBudget/>
+           {budgetList.map((budget,index)=>(
+            <BudgetItem budget={budget} />
+           ))}
         </div>
     </div>
   )
--- Frontend/expense-tracker/utils/schema.jsx
+++ Frontend/expense-tracker/utils/schema.jsx
@@ -1,5 +1,5 @@
 
-import { serial, text, varchar, pgTable } from "drizzle-orm/pg-core";
+import { serial, numeric, varchar, pgTable, integer } from "drizzle-orm/pg-core";
 
 export const Budgets = pgTable('budgets', {
     id: serial('id').primaryKey(),
@@ -7,4 +7,12 @@ export const Budgets = pgTable('budgets', {
     amount: varchar('amount').notNull(),
     icon: varchar('icon', { length: 255 }),
     createdBy: varchar('created_by', { length: 255 }).notNull()
+})
+
+export const Expenses = pgTable('expenses', {
+    id: serial('id').primaryKey(),
+    name: varchar('name').notNull(),
+    amount: varchar(numeric('amount')).notNull(),
+    createdAt: varchar('createdAt').notNull(),
+    budgetId: integer('budgetId').references(()=> Budgets.id)
 })
