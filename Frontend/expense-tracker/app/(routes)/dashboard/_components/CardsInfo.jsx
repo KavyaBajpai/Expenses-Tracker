9f019f73ebaa03eccf7727a75b45f5f57c30f88e
KavyaBajpai <kavyabajpai05@gmail.com>
2025-04-04 20:48:29 +0530

    adding cards to dashboard

---
+++ Frontend/expense-tracker/app/(routes)/dashboard/_components/CardsInfo.jsx
@@ -0,0 +1,64 @@
+import { PiggyBank, Receipt, Wallet } from 'lucide-react'
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { db } from '@/utils/dbConfig'
+
+function CardsInfo({budgetList}) {
+  const [totalBudget, setTotalBudget] = useState(0)
+  const [totalSpend, setTotalSpend] = useState(0)
+  console.log({budgetList})
+  const calcCardInfo = () => {
+      console.log("hello from cardsInfo.jsx")
+      
+      let totalBudget_=0
+      let totalSpend_ =0
+      budgetList.forEach( element => {
+         totalBudget_ = totalBudget_ + Number(element.amount)
+         totalSpend_ = totalSpend_ + element.totalSpend
+      })
+      setTotalBudget( totalBudget_ )
+      setTotalSpend( totalSpend_)
+  }
+
+  useEffect(() => {
+    budgetList&&calcCardInfo()
+ })
+  
+  return (
+    <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='p-7 border rounded-lg'>
+        <div className='flex items-center justify-between'>
+        <h2 className='text-sm'>Total Budget</h2>
+        <h2 className='text-2xl font-bold'>${totalBudget}</h2>
+        </div>
+        <PiggyBank className='bg-indigo-600 p-3 h-12 w-12 rounded-full text-white'/>
+      </div>
+
+      <div className='p-7 border rounded-lg'>
+        <div className='flex items-center justify-between'>
+        <h2 className='text-sm'>Amount Spent</h2>
+        <h2 className='text-2xl font-bold'>${totalSpend}</h2>
+        </div>
+        <Receipt className='bg-indigo-600 p-3 h-12 w-12 rounded-full text-white'/>
+      </div>
+
+      <div className='p-7 border rounded-lg'>
+        <div className='flex items-center justify-between'>
+        <h2 className='text-sm'>Amount Remaining</h2>
+        <h2 className='text-2xl font-bold'>${totalBudget - totalSpend}</h2>
+        </div>
+        <Wallet className='bg-indigo-600 p-3 h-12 w-12 rounded-full text-white'/>
+      </div>
+
+      {/* <div className='p-7 border rounded-lg'>
+        <div className='flex items-center justify-between'>
+        <h2 className='text-sm'>No. of Budgets</h2>
+        <h2 className='text-2xl font-bold'>${budgetList.length}</h2>
+        </div>
+        <Wallet className='bg-indigo-600 p-3 h-12 w-12 rounded-full text-white'/>
+      </div> */}
+    </div>
+  )
+}
+
+export default CardsInfo
--- Frontend/expense-tracker/app/(routes)/dashboard/page.jsx
+++ Frontend/expense-tracker/app/(routes)/dashboard/page.jsx
@@ -1,10 +1,48 @@
+"use client"
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
-
+import { useUser } from '@clerk/nextjs'
+import CardsInfo from './_components/CardsInfo';
+import { useEffect, useState } from 'react'
+import { sql, eq, desc } from 'drizzle-orm';
+import { Budgets, Expenses } from '@/utils/schema';
+import { db } from '@/utils/dbConfig'
+import { getTableColumns } from 'drizzle-orm/utils';
 function Dashboard() {
+
+  const { user } = useUser();
+  const [budgetList, setBudgetList] = useState([])
+
+  useEffect(() => {
+    getBudgetList()
+  }, [user])
+  const getBudgetList = async () => {
+
+    const result = await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend: sql`sum(${Expenses.amount}::integer)`.mapWith(Number),
+      totalItem: sql`count(${Expenses.id})`.mapWith(Number)
+    }).from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user.id))
+      .groupBy(Budgets.id)
+      
+    setBudgetList(result);
+    console.log("hi from page.jsx of dashboard")
+    console.log("budgetlist is")
+    console.log(result)
+  }
   return (
-    <div>
-      Dashboard hi
+    <div className='p-8'>
+      <h2 className='font-bold text-3xl'>Hi, {user?.fullName}!</h2>
+      <p className='text-gray-500 mt-2'>Let's dive in!</p>
+      <CardsInfo budgetList={budgetList} />
+
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-6'>
+        <div className='md:col-span-2'>
+          
+        </div>
+      </div>
     </div>
   )
 }
