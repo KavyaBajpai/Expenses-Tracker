1085e713d78464387b130fbfbd1a753f37c42212
KavyaBajpai <kavyabajpai05@gmail.com>
2025-04-11 23:57:23 +0530

    still checking

--- Frontend/expense-tracker/app/page.js
+++ Frontend/expense-tracker/app/page.js
@@ -1,13 +1,18 @@
-import Image from "next/image";
+"use client"
+
+import { Suspense } from "react";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      {/* <Header />
-      <Hero /> */}
-      Hello World!
+      <Suspense fallback={<div>Loading header...</div>}>
+        <Header />
+      </Suspense>
+      <Suspense fallback={<div>Loading content...</div>}>
+        <Hero />
+      </Suspense>
     </div>
   );
 }
